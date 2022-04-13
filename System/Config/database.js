9801fef3cc78220acb94ae748244553f4e356fcd
RAYDENFLY <raydenfly84@gmail.com>
2022-04-14 00:49:24 +0700

    YurikaCL: test

--- System/Config/database.js
+++ System/Config/database.js
@@ -1,7 +1,12 @@
 module.exports = {
+    connectionLimit     : 1000,
+    connectTimeout      : 60 * 60 * 1000,
+    acquireTimeout      : 60 * 60 * 1000,
+    timeout             : 60 * 60 * 1000,
     multipleStatements  : true,
     host                : 'localhost',
     user                : 'root',
     password            : '',
-    database            : 'db_node_login'
+    database            : 'db_node_login',
+    port                : '5050'
 };
